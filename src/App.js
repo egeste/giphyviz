03179c5ea45932806c3874cbc3e7a9a295bd0009
Steve Regester <git@egeste.net>
2019-04-29 23:51:51 -0700

    Remove build again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import qs from 'qs'
 import fetch from 'isomorphic-fetch'
+import chunk from 'lodash/chunk'
 import debounce from 'lodash/debounce'
 
 import { scaleLinear, scaleQuantize } from 'd3-scale'
@@ -15,10 +16,18 @@ import {
   Container, Row, Col
 } from 'react-bootstrap'
 
+import {
+  VerticalBarSeries,
+  FlexibleXYPlot
+} from 'react-vis'
+
+import Slider from 'rc-slider'
+
 import Cube from './components/Cube'
 import AudioVisualiser from './components/AudioVisualiser'
 
 import './App.css'
+import 'rc-slider/assets/index.css'
 
 const DEFAULT_FILTER = {
   api_key: 'jjIUQzslADPjJkPXJdmHrkqvGER7KVwV'
@@ -34,7 +43,8 @@ export default class App extends PureComponent {
     mp3: 'http://51.15.76.3:80/pulstranceHD.mp3',
     gifs: [],
     term: 'acid',
-    gifCount: 50,
+    freqs: [],
+    gifCount: 20,
     loadingMusic: false,
     loadingVideos: false,
     searchingGiphy: false,
@@ -47,6 +57,10 @@ export default class App extends PureComponent {
 
   onChangeSearch = ({ target }) => {
     this.setState({ term: target.value }, this.onSearchGiphy)
+  }
+
+  onChangeGifCount = gifCount => {
+    this.setState({ gifCount }, this.onSearchGiphy)
   }
 
   onChangeMP3 = debounce(({ target }) => {
@@ -76,25 +90,51 @@ export default class App extends PureComponent {
   }
 
   renderFrequencyData = freqs => {
-    if (this.state.searchingGiphy) return null
-    if (this.state.loadingVideos) return null
-    if (!this.state.gifs.length) return null
+    const shouldRenderVisualization = (
+      !this.state.searchingGiphy &&
+      !this.state.loadingVideos &&
+      this.state.gifs.length
+    )
 
-    const averageAmplitude = freqs.reduce(((memo, freq) => memo + freq), 0) / freqs.length
-    const highestAmplitudeIndex = freqs.indexOf(([ ...freqs ].sort()[0]))
+    const chunkedFreqs = chunk(freqs, 10).reduce((memo, freqsChunk) => {
+      const chunkAverage = freqsChunk.reduce(((memo, freq) => memo + freq), 0)
+      return [ ...memo, chunkAverage ]
+    }, [])
 
-    console.log(highestAmplitudeIndex)
+    const averageAmplitude = chunkedFreqs.reduce(((memo, freq) => memo + freq), 0) / chunkedFreqs.length
+    const highestAmplitude = [ ...chunkedFreqs ].sort().reverse()[0]
+    const highestAmplitudeIndex = chunkedFreqs.indexOf(highestAmplitude)
+    console.log({ highestAmplitude, highestAmplitudeIndex })
 
-    const amplitudeGif = scaleQuantize().domain([ 0, freqs.length ]).range(this.state.gifs)(highestAmplitudeIndex)
-    const zoom = frequencyZoomScalar(averageAmplitude)
+    const data = chunkedFreqs.map((freq, index) => ({
+      x: index,
+      y: freq,
+      colorType: 'literal',
+      color: (index === highestAmplitudeIndex) ? 'green' : 'purple'
+    }))
+
     return (
       <div style={ { position: 'relative' } }>
-        <img src={ `${amplitudeGif.images.original.url}` }
-          style={ { position: 'absolute', width: '100%', height: '60vh', zIndex: -1, objectFit: 'fill' } }
-        />
-        {/*
-        <Cube zoom={ zoom } style={ { width: '100%', height: '60vh' } } />
-        */}
+        <FlexibleXYPlot height={ 100 }>
+          <VerticalBarSeries colorType="literal" data={ data } />
+        </FlexibleXYPlot>
+        { shouldRenderVisualization ? (() => {
+          // const zoom = frequencyZoomScalar(averageAmplitude)
+          const amplitudeGif = scaleQuantize()
+            .domain([ 0, chunkedFreqs.length ])
+            .range(this.state.gifs)(highestAmplitudeIndex)
+
+          return (
+            <Fragment>
+              <img src={ `${amplitudeGif.images.original.url}` }
+                style={ { position: 'absolute', width: '100%', height: '60vh', zIndex: -1, objectFit: 'fill' } }
+              />
+              { /*
+              <Cube zoom={ zoom } style={ { width: '100%', height: '60vh' } } />
+              */ }
+            </Fragment>
+          )
+        })() : null }
       </div>
     )
   }
@@ -113,36 +153,39 @@ export default class App extends PureComponent {
             <Col xs={ 3 }>
               <Card>
                 <Card.Body>
-                  <Form>
-
-                    <Form.Group>
-                      <Form.Label children="Audio Source" />
+                  <Form.Group>
+                    <Form.Label children="Audio Source" />
+                    <Form.Control type="text"
+                      value={ this.state.mp3 }
+                      placeholder="http://someurl.com/file.mp3"
+                      onChange={ this.onChangeMP3 }
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label children="Giphy Search Term" />
+                    <InputGroup>
                       <Form.Control type="text"
-                        value={ this.state.mp3 }
-                        placeholder="http://someurl.com/file.mp3"
-                        onChange={ this.onChangeMP3 }
+                        value={ this.state.term }
+                        placeholder="Search"
+                        onChange={ this.onChangeSearch }
                       />
-                    </Form.Group>
-
-                    <Form.Group>
-                      <Form.Label children="Giphy Search Term" />
-                      <InputGroup>
-                        <Form.Control type="text"
-                          value={ this.state.term }
-                          placeholder="Search"
-                          onChange={ this.onChangeSearch }
-                        />
-                        { this.state.searchingGiphy ? (
-                          <InputGroup.Append>
-                            <InputGroup.Text>
-                              <Spinner animation="border" size="sm" />
-                            </InputGroup.Text>
-                          </InputGroup.Append>
-                        ) : null }
-                      </InputGroup>
-                    </Form.Group>
-
-                  </Form>
+                      { this.state.searchingGiphy ? (
+                        <InputGroup.Append>
+                          <InputGroup.Text>
+                            <Spinner animation="border" size="sm" />
+                          </InputGroup.Text>
+                        </InputGroup.Append>
+                      ) : null }
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label children="Number of gifs" />
+                    <InputGroup>
+                      <Slider defaultValue={ this.state.gifCount }
+                        onChange={ this.onChangeGifCount }
+                      />
+                    </InputGroup>
+                  </Form.Group>
                 </Card.Body>
               </Card>
             </Col>
